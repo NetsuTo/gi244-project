756c9495f84920bfab42f700d4f29d00f2b0ae51
NetsuTo <narawichsonwai1z@gmail.com>
2024-04-30 23:17:49 +0700

    Project

---
+++ Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectionMarket;
+    public GameObject SelectionMarket {  get { return selectionMarket; } }
+
+    [SerializeField] private TextMeshProUGUI unitCountText;
+    [SerializeField] private TextMeshProUGUI foodText;
+    [SerializeField] private TextMeshProUGUI woodText;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI stoneText;
+
+    [SerializeField] private RectTransform selectionBox;
+    public RectTransform SelectionBox { get { return selectionBox; } }
+
+    private Canvas canvas;
+    public Canvas Canvas { get { return canvas; } }
+
+    public static MainUI instance;
+
+    private void Awake()
+    {
+        instance = this;
+        canvas = GetComponent<Canvas>();
+    }
+
+    public void UpdateAllResource(Faction faction)
+    {
+        unitCountText.text = $"{faction.AliveUnits.Count}/{faction.UnitLimit}";
+        foodText.text = faction.Food.ToString();
+        woodText.text = faction.Wood.ToString();
+        goldText.text = faction.Gold.ToString();
+        stoneText.text = faction.Stone.ToString();
+    }
+
+    public Vector3 ScalePosition(Vector3 pos)
+    {
+        Vector3 newPos;
+
+        newPos = new Vector3(pos.x * canvas.transform.localScale.x
+                             , pos.y * canvas.transform.localScale.y
+                             , pos.z * canvas.transform.localScale.z);
+
+        return newPos;
+    }
+}
